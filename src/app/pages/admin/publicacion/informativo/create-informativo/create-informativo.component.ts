01bd59852fdbb26786a06a5d52d468caaf155f2a
Jeans <sistemadesignstyle@gmail.com>
2024-08-01 01:16:44 -0500

    Avance crud informativo

--- src/app/pages/admin/publicacion/informativo/create-informativo/create-informativo.component.ts
+++ src/app/pages/admin/publicacion/informativo/create-informativo/create-informativo.component.ts
@@ -1,12 +1,183 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Informativo } from 'src/app/models/informativo.model';
+import { InformativoService } from 'src/app/services/informativo.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { environment } from 'src/environments/environment';
+/*  1 - Quill */
+import { QuillModule } from 'ngx-quill';
+/*  /1 - Quill  */
 
 @Component({
   selector: 'app-create-informativo',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    /* CKEditorModule, */
+    /*  2 - Quill */
+    QuillModule,
+    /*  2 - Quill */
+  ],
   templateUrl: './create-informativo.component.html',
-  styleUrl: './create-informativo.component.css'
+  styleUrl: './create-informativo.component.css',
 })
 export class CreateInformativoComponent {
+  listCategories: any = [];
+  files_date: any;
+  submitted = false;
+  data: any;
+  form: FormGroup = new FormGroup({});
+  urlRaiz = environment.urlRaiz + '/';
+  post = new Informativo();
+  htmlContent: any;
 
+  moduleQuill = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ header: 1 }, { header: 2 }], // custom button values
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+      [{ direction: 'rtl' }], // text direction
+
+      [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ font: [] }],
+      [{ align: [] }],
+
+      ['clean'], // remove formatting button
+
+      ['link', 'image', 'video'], // link and image, video
+    ],
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: InformativoService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.loadCategories();
+  }
+
+  onChangeEditor(event: any): void {
+    if (event.html) {
+      this.htmlContent = event.html;
+    }
+  }
+
+  loadCategories() {
+    return this.dataService.getInformativos().subscribe((data: {}) => {
+      console.log(data);
+      this.listCategories = data;
+    });
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      titulo: [null, Validators.required],
+      resumen: [null, Validators.required],
+      descripcion: [null, Validators.required],
+      fuente: [null, Validators.required],
+      image: [null, Validators.required],
+      autor: [null, Validators.required],
+      destacado: [false],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  uploadImage(event: Event) {
+    if (event.target instanceof HTMLInputElement) {
+      if (event.target.files && event.target.files.length > 0) {
+        const files = event.target.files[0];
+        this.files_date = files;
+        const maxSizeInBytes = 5 * 1024 * 1024; // 5MB
+
+        if (files.size > maxSizeInBytes) {
+          console.log('La imagen excede el tamaño máximo permitido (5MB)');
+          this.alertaMaxFile();
+          // Puedes mostrar un mensaje de error o realizar otra acción
+          event.target.value = ''; // Limpiar el input file
+          return;
+        }
+
+        if (!['image/jpeg', 'image/png'].includes(files.type)) {
+          console.log('Solo se permiten archivos JPG y PNG');
+          this.alertaExtFile();
+          // Puedes mostrar un mensaje de error o realizar otra acción
+          event.target.value = ''; // Limpiar el input file
+          return;
+        }
+
+        // Aquí puedes continuar con el proceso de carga de la imagen
+        console.log('Archivo seleccionado:', files);
+      } else {
+        console.log('No se seleccionó ningún archivo');
+      }
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('titulo', this.form.value.titulo);
+    formData.append('resumen', this.form.value.resumen);
+    formData.append('descripcion', this.form.value.descripcion);
+    formData.append('fuente', this.form.value.fuente);
+    formData.append('imagen', this.files_date, this.files_date.name);
+    formData.append('autor', this.form.value.autor);
+    formData.append('destacado', this.form.value.destacado);
+    this.dataService.uploadData(formData).subscribe((res) => {
+      this.data = res;
+      console.log(this.data);
+      this.alerta();
+    });
+  }
+
+  alerta() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Imagen subida',
+    });
+  }
+
+  alertaMaxFile() {
+    Swal.fire({
+      icon: 'error',
+      title: 'La imagen excede el tamaño máximo permitido (5MB)',
+    });
+  }
+
+  alertaExtFile() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Solo se permiten archivos JPG y PNG',
+    });
+  }
+
+  alertaDelete() {
+    Swal.fire({
+      icon: 'success',
+      title: 'Registro eliminado',
+    });
+  }
 }
